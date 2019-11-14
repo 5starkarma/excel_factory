3dd3e4c7ccb6613a7e6ba1139b11d6e8e72d5105
David Alford <davidalford678@gmail.com>
2019-11-14 07:51:40 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='excel_factory',
+      version='0.1',
+      description='A module for extracting the data from an Excel '
+                  'spreadsheet regardless of the Excel file type '
+                  '(XLS or XLSX). Output is a Pandas data-frame.',
+      url='https://github.com/5starkarma/excel_factory.git',
+      author='David Alford',
+      author_email='davidalford678@gmail.com',
+      license='MIT',
+      packages=['excel_factory'],
+      zip_safe=False)
